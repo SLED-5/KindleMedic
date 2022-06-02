7d99fe040ef61738582084024b6f167ea0720458
Gabriel Zhang <57888590+SLED-5@users.noreply.github.com>
2022-06-02 22:19:01 +0800

    Add files via upload

---
+++ KindleMedic.js
@@ -0,0 +1,20 @@
+var curIdx = 0;
+function kindleMedic () {
+    curIdx++;
+    $('input.hideItem_myx')[curIdx].click();
+    $('ul button.myx-button-text')[curIdx].click();
+    console.log("Downloading:".concat(curIdx + 1).concat(" out of ").concat($('input.hideItem_myx').length));
+    setTimeout(function () { $('.myx-popover-inner .transcludedContent_myx .contentDetails_myx #contentAction_download_myx').trigger('click'); }, 1000);
+    setTimeout(function () { $('.dialog_myx .myx-button-primary').trigger('click'); }, 2000);
+    /* 
+    You can change the timeout below to start downloading new book faster.
+    But some books may fail to download with a very short timeout.
+    */
+    if (curIdx < $('input.hideItem_myx').length){
+        setTimeout(kindleMedic, 30000);
+    } else {
+        window.alert("RIP Kindle in China 2013 - 2022")
+    }
+   
+}
+kindleMedic();
